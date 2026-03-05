3d6cfc4d974af204c4dd450344dde2a4c484d6fe
Kira <kira-ismailova@bk.ru>
2026-03-05 13:18:32 +0300

    доработки после ревью

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,15 @@
 import { restaurants } from "../../materials/mock";
+import { RestaurantCard } from "./restaurant/RestaurantCard";
 
 export const App = () => {
   return (
     <div>
       {restaurants.map((restaurant, index) => (
-        <div key={restaurant.id}>
-          <h1>Ресторан № {index + 1} - {restaurant.name}</h1>
-          <h3>Меню:</h3>
-          <div>
-            {restaurant.menu.map((itemMenu) => (
-              <ul key={itemMenu.id}>
-                <li>{itemMenu.name}</li>
-              </ul>
-            ))}
-          </div>
-          <h3>Отзывы:</h3>
-          <div>
-            {restaurant.reviews.map((itemReview) => (
-              <ul key={itemReview.id}>
-                <li>{itemReview.text}</li>
-              </ul>
-            ))}
-          </div>
-        </div>
+        <RestaurantCard 
+          key={restaurant.id} 
+          restaurant={restaurant} 
+          index={index} 
+        />
       ))}
     </div>
   );
