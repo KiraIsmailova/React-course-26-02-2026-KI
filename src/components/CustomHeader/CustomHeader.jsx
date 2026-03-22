f6cc5acb2b666653f7408449c137341674f503b1
Kira <kira-ismailova@bk.ru>
2026-03-22 18:19:19 +0300

    hw-5 finish

--- src/components/CustomHeader/CustomHeader.jsx
+++ src/components/CustomHeader/CustomHeader.jsx
@@ -1,9 +1,11 @@
 import styles from './CustomHeader.module.css';
 import { useContext } from 'react';
 import { ThemeContext } from '../ThemeContext/ThemeContext';
+import { AuthContext } from '../AuthContext/AuthContext';
 
 export const CustomHeader = () => {
   const { isDark, toggleTheme } = useContext(ThemeContext);
+  const { user, isAuthenticated, login, logout } = useContext(AuthContext);
 
   return (
     <header className={styles['customHeader']}>
@@ -13,6 +15,21 @@ export const CustomHeader = () => {
         <button onClick={toggleTheme} className={styles.toggleBtn}>
           {isDark ? '☀️ Переключить на светлую' : '🌙 Переключить на темную'}
         </button>
+
+        {isAuthenticated ? (
+          <div className={styles.userBlock}>
+            <span className={styles.userName}>
+              {user.avatar} {user.name}
+            </span>
+            <button onClick={logout} className={styles.logoutBtn}>
+              Выйти
+            </button>
+          </div>
+        ) : (
+          <button onClick={login} className={styles.loginBtn}>
+            Войти
+          </button>
+        )}
         <ul className={styles['headerList']}>
           <li>item 1</li>
           <li>item 2</li>
